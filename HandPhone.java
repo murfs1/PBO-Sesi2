8a3082ef211026056f4c8c01f24ea616350f2f74
Muhamad Rifda Firdaus. S <102627725+murfs1@users.noreply.github.com>
2024-02-27 10:58:50 +0700

    Add files via upload

---
+++ HandPhone.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author ASUS
+ */
+public class HandPhone {
+    
+    String jenis_hp;
+    int tahun_pembuatan;
+    
+    public void setDataHP(String jenis_hp, int tahun_pembuatan) {
+        this.jenis_hp = jenis_hp;
+        this.tahun_pembuatan = tahun_pembuatan;
+    }
+    
+    
+    public String getJenisHP () {
+        return jenis_hp;
+    }
+   
+    public int getTahunPembuatan () {
+        return tahun_pembuatan;
+    }
+
+    public static void main(String args[]){
+        HandPhone hp = new HandPhone ();
+        hp.setDataHP("Samsung", 2004);
+        System.out.println(hp.getJenisHP());
+        System.out.println(hp.getTahunPembuatan());
+    }
+}
